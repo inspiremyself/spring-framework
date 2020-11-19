b1d687c486c4617e364aaacd6315653edf467b22
yongliang <inspiremyself@sohu.com>
2020-11-19 11:39:37 +0800

    test config

---
+++ abspring-study/src/main/java/com/langme/service/JavaConfig.java
@@ -0,0 +1,15 @@
+package com.langme.service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class JavaConfig {
+
+	@Bean
+	public User user(){
+		return new User("001","smart leon");
+	}
+}
---
+++ abspring-study/src/main/java/com/langme/service/Test.java
@@ -0,0 +1,13 @@
+package com.langme.service;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("haha");
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(JavaConfig.class);
+		User user = (User)context.getBean("user");
+		System.out.println(user.toString());
+
+	}
+}
